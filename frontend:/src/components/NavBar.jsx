2b98064cf53580832f494e3deb8782bb6fbfec1c
SharonRandhawa <sharon.randhawa@ucdconnect.ie>
2025-05-23 13:20:07 +0100

    Add Vite React frontend scaffold in /frontend folder (#1)

---
+++ frontend:/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+      <nav style={{ padding: '1rem', background: '#eee' }}>
+        <a href="/">Home</a> | <a href="/map">Map</a> | <a href="/predict">Predict</a>
+      </nav>
+    );
+  }
+  
+  export default NavBar;
