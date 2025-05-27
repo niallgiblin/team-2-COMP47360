00c0bee59b7cd6a86e94815492cd15cf3b8a7b3c
SharonRandhawa <sharon.randhawa@ucdconnect.ie>
2025-05-27 14:30:26 +0100

    Setting up features and testing adding a branch

---
+++ frontend:/src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h2>About The Urban Gala</h2>
+        <p>Coming soon...</p>
+      </div>
+    );
+  }
---
+++ frontend:/src/pages/VenueDetail.jsx
@@ -0,0 +1,8 @@
+export default function VenueDetail() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h2>Venue Detail</h2>
+        <p>Coming soon...</p>
+      </div>
+    );
+  }
