14d470958b9b25b3f3651ba27c77e4cf2c3acc9d
Niall Giblin <90722810+niallgiblin@users.noreply.github.com>
2025-06-03 10:37:41 +0100

    Backend (#4)
    * deployment of springboot
    
    * Create Backend Dockerfile
    
    * Add forgotten .dockerignore
    
    ---------
    
    Co-authored-by: Ruiqi-programer <ruiqi.guo1@ucdconnect.ie>

---
+++ BackEnd/busyness_predictor/src/main/java/com/manhattan/busyness_predictor/BusynessPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.manhattan.busyness_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusynessPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusynessPredictorApplication.class, args);
+	}
+
+}
