d8bccff566d39055cdea8d4c0eb8c3dcc54149fd
SharonRandhawa <sharon.randhawa@ucdconnect.ie>
2025-06-05 13:55:36 +0100

    Updated map view page with venue card components

--- frontend/src/components/DemoMap.jsx
+++ frontend/src/components/DemoMap.jsx
@@ -1,19 +1,32 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Box } from '@mui/material';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'; // Components for rendering the map and it's elements
+import { Box } from '@mui/material'; // Box is a layout component from MUI
 
+// Define the DemoMap component
 export default function DemoMap() {
   return (
-    <Box sx={{ width: '100%', height: '500px', my: 4 }}>
+    // Use MUI's box component as a containere
+    <Box 
+      sx={{ 
+        width: '100%', 
+        height: '100%', 
+        }}>
+      
+      {/* Initialise the map container */}
       <MapContainer
-        center={[40.7128, -74.006]} // Manhattan
-        zoom={13}
-        style={{ height: '100%', width: '100%' }}
+        center={[40.7128, -74.006]} // Set initial centre to Manhattan
+        zoom={13} // Set initial zoom level
+        style={{ height: '100%', width: '100%' }} // Ensure the map takes up the full size of the box container
       >
+        
+        {/* Load and display tiles from OpenStreetMap */}
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
+        
+        {/* Add a marker at the some location as the map centre */}
         <Marker position={[40.7128, -74.006]}>
+          {/* Show a popup when the marker is clicked */}
           <Popup>You're in Manhattan!</Popup>
         </Marker>
       </MapContainer>
