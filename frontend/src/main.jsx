e6a3e5c50315ab9b22517f036537a1f2e4028666
SharonRandhawa <sharon.randhawa@ucdconnect.ie>
2025-06-06 16:41:13 +0100

    Implement Find My Vibe page, What's Hot page, other UI improvements

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'leaflet/dist/leaflet.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 
+// Create a custom MUI theme using Urbanist font
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Urbanist, sans-serif',
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
+
+
