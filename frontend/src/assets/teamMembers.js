e7ac769c36e952b99c22d591c45f0020ecdcf4eb
SharonRandhawa <sharon.randhawa@ucdconnect.ie>
2025-06-13 11:58:55 +0100

    Front end (#14)
    * Update hero section, feature cards layout, and skyline positioning
    
    * Updated map view page with venue card components
    
    * Implement Find My Vibe page, What's Hot page, other UI improvements
    
    * Add .DS_Store to .gitignore
    
    * Remove .DS_Store and add to .gitignore
    
    * Connect frontend to backend APIs
    
    * Remove error log file from repo
    
    * Finish auth UI setup and context
    
    * Updated map with zones using GeoJSON
    
    * Add About page, image assets, map flytovenue
    
    ---------
    
    Co-authored-by: Niall Giblin <90722810+niallgiblin@users.noreply.github.com>

---
+++ frontend/src/assets/teamMembers.js
@@ -0,0 +1,43 @@
+import graceImg from './grace.jpg';
+// import sharonImg from './sharon.jpg';
+import niallImg from './niall.jpg';
+// import ruiqiImg from './ruiqi.jpg';
+// import gregoryImg from './gregory.jpg';
+// import johnImg from './john.jpg';
+
+const teamMembers = [
+    {
+        name: 'Grace',
+        image: graceImg,
+        bio: 'Grace visited New York with friends during her J1 and loves spending time in big cities',
+      },
+    {
+      name: 'Sharon',
+      image: '/assets/sharon.jpg',
+      bio: 'Sharon has lived in cities all over the world and loves discovering hidden gems in each one — from a cozy tea shop in Budapest, to a rooftop nightclub in Kuala Lumpur. An app like The Urban Gala is perfect for navigating a bustling city and planning the ultimate night out.',
+    },
+    {
+      name: 'Niall',
+      image: niallImg,
+      bio: 'Niall lived in NYC for 5 years and explored all 5 boroughs extensively by bike, foot, and train.',
+    },
+    {
+        name: 'Ruiqi',
+        image: '/assets/ruiqi.jpg',
+        bio: '',
+    },
+    {
+        name: 'Gregory',
+        image: '/assets/gregory.jpg',
+        bio: '',
+    },
+    {
+        name: 'John',
+        image: '/assets/john.jpg',
+bio: '',
+      },
+   
+  ];
+  
+  export default teamMembers;
+  
