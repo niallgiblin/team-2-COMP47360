b5c8e6cfc3819c3bac32a36be6d88df462eaf33d
SharonRandhawa <sharon.randhawa@ucdconnect.ie>
2025-06-19 10:12:20 +0100

    Changed Docker files for live development

--- BackEnd/src/main/java/com/manhattan/busyness_predictor/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/manhattan/busyness_predictor/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // Allow cookies if needed
